17bff2e8371c5255046b9f19b1401bf4eac2fa4d
JamesKemp296 <jameskemp2796@yahoo.com>
2020-01-04 18:51:02 -0500

    fixed loop direction ti increment up

--- Problem-1.js
+++ Problem-1.js
@@ -6,10 +6,10 @@
 
 function multiplesOf3and5(number) {
   let sum = 0
-  for (i = number; i > 0; i--) {
+  for (i = 0; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0) sum += i
   }
-  return sum
+  console.log(sum)
 }
 
 multiplesOf3and5(1000) // 233168
