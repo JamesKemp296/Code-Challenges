ea3cdf7a24fe58bf54b0b7965c36179e1cff7ee4
JamesKemp296 <jameskemp2796@yahoo.com>
2021-05-19 17:00:32 -0400

    8kyu codewars

---
+++ Code Wars/8 kyu/Replace-All-Dots.js
@@ -0,0 +1,6 @@
+// The code provided is supposed replace all the dots .
+// in the specified String str with dashes -
+// But it's not working properly.
+// Fix the bug so we can all go home early.
+
+const replaceDots = (str) => str.replace(/\./g, '-')
