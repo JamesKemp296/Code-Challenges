e882501fef35c4af7c9cc76949ecf702c502d89f
James <jameskemp2796@yahoo.com>
2021-06-10 12:47:59 -0400

    Codewars 8kyu

---
+++ Code Wars/8 kyu/Check-Alive.js
@@ -0,0 +1,12 @@
+// While making a game, your partner, Greg, decided to
+// create a function to check if the user is still alive
+// called checkAlive. Unfortunately, Greg made some errors
+// while creating the function.
+
+// checkAlive should return true if the player's health is
+// greater than 0 or false if it is 0 or below.
+
+// The function receives one parameter health which will always
+// be a whole number between -10 and 10.
+
+const checkAlive = (health) => health > 0
