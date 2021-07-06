b1cd432aa853a2012d1ad1382cad09812ec2024e
James <jameskemp2796@yahoo.com>
2021-07-06 11:02:14 -0400

    Codewars 8kyu

---
+++ Code Wars/JavaScript/8 kyu/Convert-Num-To-Reversed-Arr-Of-Nums.js
@@ -0,0 +1,8 @@
+// Given a random non-negative number, you have to return
+// the digits of this number within an array in reverse order.
+
+const digitize = (n) =>
+  String(n)
+    .split("")
+    .reverse()
+    .map((num) => Number(num))
