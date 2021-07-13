d8ed33c8406efabdb84233ae87b9bc76340c670a
JamesKemp296 <jameskemp2796@yahoo.com>
2021-07-13 10:14:29 -0400

    Codewars 8kyu

---
+++ Code Wars/JavaScript/8 kyu/Multiplication-Table.js
@@ -0,0 +1,10 @@
+// Your goal is to return multiplication table for number that 
+// is always an integer from 1 to 10.
+
+function multiTable(number) {
+  let myArr = []
+  for (let i = 1; i < 11; i++) {
+     myArr.push(`${i} * ${number} = ${i*number}`)
+  }
+  return myArr.join('\n')
+}
