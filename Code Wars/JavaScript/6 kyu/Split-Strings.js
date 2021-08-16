a2b2b2d8d9407bc2dfa1291eadc9ed83c7710ff2
James <jameskemp2796@yahoo.com>
2021-08-16 11:30:35 -0400

    Codewars 6kyu

---
+++ Code Wars/JavaScript/6 kyu/Split-Strings.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it splits the string into pairs of
+// two characters. If the string contains an odd number of characters
+// then it should replace the missing second character of the final pair
+// with an underscore ('_').
+
+// Examples:
+// solution('abc') // should return ['ab', 'c_']
+// solution('abcdef') // should return ['ab', 'cd', 'ef']
+
+function solution(str) {
+  if (!str) return []
+  if (str.length % 2 !== 0) str += "_"
+  return str.match(/.{1,2}/g)
+}
