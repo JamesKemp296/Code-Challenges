e0559ccc41f9808cbc942c3ee2c7dfd2554f25b7
James <jameskemp2796@yahoo.com>
2021-09-27 20:25:00 -0400

    Codewars 8kyu

---
+++ Code Wars/JavaScript/8 kyu/Sum-Without-High-Low-Nums.js
@@ -0,0 +1,11 @@
+// Sum all the numbers of the array (in F# and Haskell you get a list)
+// except the highest and the lowest element (the value, not the index!).
+// (The highest/lowest element is respectively only one element at each edge,
+// even if there are more than one with the same value!)
+
+function sumArray(array) {
+  if (!array || array.length <= 1) return 0
+  array.sort((a, b) => a - b).pop()
+  array.shift()
+  return array.reduce((a, b) => a + b, 0)
+}
