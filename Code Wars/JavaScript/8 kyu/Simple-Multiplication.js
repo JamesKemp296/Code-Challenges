58669843ea7d966f64922652a0d74a5a3754a607
James <jameskemp2796@yahoo.com>
2021-10-11 19:59:32 -0400

    Codewars 8kyu

---
+++ Code Wars/JavaScript/8 kyu/Simple-Multiplication.js
@@ -0,0 +1,6 @@
+// Multiplying a given number by eight if it is an even number
+// and by nine otherwise.
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9
+}
