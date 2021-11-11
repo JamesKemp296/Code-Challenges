2135f7d928bfbb4e1245757db89e8666ce85697e
JamesKemp296 <jameskemp2796@yahoo.com>
2021-11-11 08:47:30 -0500

    Codewars 8kyu

---
+++ Code Wars/JavaScript/8 kyu/Filter-Geese.js
@@ -0,0 +1,14 @@
+// Write a function, gooseFilter, that takes an array of strings
+// as an argument and returns a filtered array containing the same
+// elements but with the 'geese' removed.
+
+function gooseFilter(birds) {
+  const geese = [
+    'African',
+    'Roman Tufted',
+    'Toulouse',
+    'Pilgrim',
+    'Steinbacher',
+  ]
+  return birds.filter((bird) => !geese.includes(bird))
+}
